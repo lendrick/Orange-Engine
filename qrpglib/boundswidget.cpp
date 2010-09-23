e3f7465cc784ad78398fac8d449eff20860cf6af
bart <elbarto@gmail.com>
2010-09-23 19:35:45 -0400

    stuff

--- qrpglib/boundswidget.cpp
+++ qrpglib/boundswidget.cpp
@@ -1,6 +1,24 @@
 #include "boundswidget.h"
+#include "coordinatewidget.h"
 
-BoundsWidget::BoundsWidget(QWidget *parent) :
-    QFrame(parent)
+BoundsWidget::BoundsWidget(QWidget *parent, int x1, int y1, int x2, int y2) :
+    QGroupBox(parent)
 {
+  setTitle("Bounds");
+
+  formLayout = new QFormLayout;
+  setLayout(formLayout);
+
+  topLeft = new CoordinateWidget(0, x1, y1);
+  formLayout->addRow("Upper left bound", topLeft);
+
+  bottomRight = new CoordinateWidget(0, x2, y2);
+  formLayout->addRow("Lower right bound", bottomRight);
+}
+
+void BoundsWidget::getBounds(int &x1, int &y1, int &x2, int &y2) {
+  x1 = topLeft.x->value();
+  y1 = topLeft.y->value();
+  x2 = bottomRight.x->value();
+  y2 = bottomRight.y->value();
 }
--- qrpglib/boundswidget.h
+++ qrpglib/boundswidget.h
@@ -4,13 +4,17 @@
 #include <QFrame>
 #include "coordinatewidget.h"
 
-class BoundsWidget : public QFrame
+class BoundsWidget : public QGroupBox
 {
   Q_OBJECT
 public:
-  explicit BoundsWidget(QWidget *parent = 0);
+  explicit BoundsWidget(QWidget *parent = 0, int x1 = 0, int y1 = 0, int x2 = 0, int y2 = 0);
+  void getBounds(int &, int &, int &, int &);
   CoordinateWidget topLeft;
   CoordinateWidget bottomRight;
+protected:
+  QFormLayout * formLayout;
+
 signals:
 
 public slots:
